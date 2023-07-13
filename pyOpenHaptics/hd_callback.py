1dc02c12d5e7f2ca61049a428849a0eee4935bf1
mikelitu <mikelitubiomed@gmail.com>
2023-07-13 16:24:33 +0100

    simplify the the callback function even more

--- pyOpenHaptics/hd_callback.py
+++ pyOpenHaptics/hd_callback.py
@@ -9,14 +9,15 @@ def hd_callback(input_function):
     @functools.wraps(input_function)
     @CFUNCTYPE(HDCallbackCode, POINTER(c_void_p))
     def _callback(pUserData):
+        begin_frame(get_current_device())
         input_function()
+        end_frame(get_current_device())
         if(get_error()):
             return HD_CALLBACK_DONE
         return HD_CALLBACK_CONTINUE
     
     return _callback
 
-    
 def hdAsyncSheduler(callback):
     pUserData = c_void_p()
     _lib_hd.hdScheduleAsynchronous(callback, byref(pUserData), HD_MAX_SCHEDULER_PRIORITY)
